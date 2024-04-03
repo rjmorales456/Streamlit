a5586aa432c0bcb4efa4ebaa389c007521c8d1af
rjmorales456 <jerard645@gmail.com>
2024-04-03 14:40:33 +0800

    added streamlit app

---
+++ Job_salaries/job_salary.py
@@ -0,0 +1,19 @@
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import warnings
+import seaborn as sns
+import holoviews as hv
+import hvplot.pandas
+import plotly.express as px
+import plotly.graph_objects as go
+import streamlit as st
+warnings.filterwarnings('ignore')
+
+
+st.write("""
+         
+         Test
+         
+         """)
